753dbb69fd8f3fb9e64f6fd0eff8f1d846739a5a
armazzer <alice.mazzer.88@gmail.com>
2023-11-23 22:22:05 +0000

    Updates db_utils.py with docustrings

--- db_utils.py
+++ db_utils.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def load_credentials():
+'''This function reads the credentials file, returning a dictionary'''
     with open("credentials.yaml", "r") as file:
         credentials = yaml.safe_load(file)
         return credentials
@@ -10,15 +11,23 @@ def load_credentials():
 credentials = load_credentials()
 
 class RDSDatabaseConnector:
+    '''This class is used to connect to the RDS database, extract the required data, and save the data in a csv file
+        Attributes:
+            credentials (dict): a dictionary of credentials required to connect to the RDS database.
+            df (dataframe): a dataframe that is initially empty, and will be updated with data extracted from the RDS database.'''
     def __init__(self, credentials):
         self.credentials = credentials
         self.df = pd.DataFrame()
 
     def read_data(self, conn):
+        '''This method reads data from a selected table, given as argument to the read_sql_table function, via the connection established in the get_data method.
+        The class attribute df is then updated with the resulting dataframe'''
         loan_payments = pd.read_sql_table('loan_payments', conn)
         self.df = loan_payments
 
-    def get_data(self): 
+    def get_data(self):
+        '''This method creates an engine to establish a connection to the RDS database. 
+        The read_data method then runs as a sub-method.''' 
         DATABASE_TYPE = "postgresql"
         DBAPI = "psycopg2"
         HOST = self.credentials["RDS_HOST"]
@@ -32,6 +41,7 @@ class RDSDatabaseConnector:
             self.read_data(conn)
 
     def make_csv(self):
+        '''This method saves the extracted data to a csv file'''
         self.df.to_csv("loan_payments.csv")
 
 
