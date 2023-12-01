f7db32f1caadde57616e673f546180f3ec56d42f
armazzer <alice.mazzer.88@gmail.com>
2023-12-01 16:31:19 +0000

    Updates dataframeinfo.py. The method column_stats has been updated to only round results when the datatype is int64 or float64

--- dataframeinfo.py
+++ dataframeinfo.py
@@ -13,6 +13,12 @@ class DataFrameInfo():
         missing_values_only = missing_values[missing_values > 0]
         print("Column               Missing values (%) \n", missing_values_only)
 
+    def count_na(self):
+         pd.set_option("display.precision", 2)
+         missing_count = self.data.isnull().sum()
+         missing_count_only = missing_count[missing_count > 0]
+         print("Column                Missing values \n", missing_count_only)
+
     def shape(self):
         shape = self.data.shape
         print(f"The shape of the dataframe or array is: {shape}")
@@ -37,10 +43,11 @@ class DataFrameInfo():
         print(col_stats)
 
     def column_stats(self, column):
+        num_types = ["int64", "float64"]
         pd.set_option("display.precision", 4)
-        mean = round(self.data[column].mean(), 3)
-        median = round(self.data[column].median(), 3)
-        mode = round(self.data[column].mode(), 3)
-        stdev = round(self.data[column].std(), 3)
+        mean = round(self.data[column].mean(), 3) if self.data[column].dtypes in num_types else self.data[column].mean()
+        median = round(self.data[column].median(), 3) if self.data[column].dtypes in num_types else self.data[column].median()
+        mode = round(self.data[column].mode(), 3) if self.data[column].dtypes in num_types else self.data[column].mode()
+        stdev = round(self.data[column].std(), 3) if self.data[column].dtypes in num_types else self.data[column].std()
         print(f"{column}: \n mean: {mean} \n median: {median} \n mode: {mode.iloc[0] if len(list(mode)) == 1 else list(mode)} \n standard deviation: {stdev}")
         
