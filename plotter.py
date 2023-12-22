6f910f9c0b1f7e8104e418245777ac86ebd5a923
armazzer <alice.mazzer.88@gmail.com>
2023-12-22 22:55:17 +0000

    Update plotter.py. Adds another method to the Plotter class

--- plotter.py
+++ plotter.py
@@ -97,6 +97,10 @@ class Plotter:
         ax1.tick_params(axis='y')
         fig.tight_layout()
 
+    def dual_prob_plot(self, col_x, col_cat, marginal="box", barnorm="fraction", w=500, h=500):
+        fig = px.histogram(self.data, x=col_x, color=col_cat, marginal=marginal, barnorm=barnorm, title='Probability Bar Plots', width=w, height=h)
+        fig.show()
+
     def bar_plot_twin_y(self, column_a, column_b, y_left, y_right, w, h):
         fig, ax1 = plt.subplots(figsize=(w, h))
 
